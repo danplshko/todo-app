27dcf9c95468acd0605dde3fa1b4a76a477d8794
Daniil Plyushko <danplshko@gmail.com>
2023-01-08 20:02:21 +0200

    Create Todo cli app.

---
+++ cli.py
@@ -0,0 +1,67 @@
+# from function import get_todos, write_todos
+import functions
+# from database.functions import get_todos, write_todos
+import time
+
+now = time.strftime("%b, %d, %Y %H:%M:%S")
+print("It is", now)
+
+while True:
+    user_action = input("Type add NewTodo, show, edit NumberOfTodo, complete NumberOfTodo or exit: ")
+    user_action = user_action.strip().lower()
+
+    if user_action.startswith("add"):
+
+        todo = user_action[4:]
+
+        todos = functions.get_todos()
+
+        todos.append((todo + '\n').capitalize())
+
+        functions.write_todos(todos)
+
+    elif user_action.startswith("show"):
+
+        todos = functions.get_todos()
+
+        for index, item in enumerate(todos):
+            item = item.strip('\n')
+            print(f"{index + 1}-{item}")
+
+    elif user_action.startswith("edit"):
+        try:
+            todos = functions.get_todos()
+
+            number = int(user_action[5:])
+            number = number - 1
+            new_todo = input("Enter new todo: ")
+            todos[number] = new_todo.capitalize() + "\n"
+
+            functions.write_todos(todos)
+        except ValueError:
+            print("Not right command")
+            continue
+
+    elif user_action.startswith("complete"):
+        try:
+            number = int(user_action[9:])
+
+            todos = functions.get_todos()
+            index = number - 1
+            todo_to_remove = todos[index].strip("\n")
+            todos.pop(index)
+
+            functions.write_todos(todos)
+
+            message = f"Todo {todo_to_remove} was removed from the list."
+            print(message)
+        except IndexError:
+            print("Not existing task. Use show command to refresh your mind.")
+            continue
+        except ValueError:
+            print("Please choose task to complete by number.")
+
+    elif user_action.startswith("exit"):
+        break
+
+print("Bye!")
